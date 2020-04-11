a0290fd46d612afd6caaee4daa6161a3bd494e88
Claire <49087389+claireskim@users.noreply.github.com>
2020-04-10 18:13:40 -0700

    Update network.py

--- network.py
+++ network.py
@@ -1,23 +1,16 @@
-import d3py # python module that generate xml pages embedding d3.js script, but not actively maintained
 import plotly.graph_objects as go
 import networkx as nx
-
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-G = nx.Graph()
-G.add_edge(1,2)
-G.add_edge(1,3)
-G.add_edge(3,2)
-G.add_edge(3,4)
-G.add_edge(4,2)
-
-# use 'with' if you are writing a script and want to serve this up forever
-
-with d3py.NetworkXFigure(G, width=500, height=500, host="localhost") as p:
-    p += d3py.ForceLayout()
-    p.show()
-    
+# G = nx.Graph()
+# G.add_edge(1,2)
+# G.add_edge(1,3)
+# G.add_edge(3,2)
+# G.add_edge(3,4)
+# G.add_edge(4,2)
+G = nx.random_geometric_graph(200, 0.125)
+ 
 edge_x = []
 edge_y = []
 for edge in G.edges():
